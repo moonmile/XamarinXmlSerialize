163dc009eafd472481a060c3cc0c6158a35c4e6d
Tomoaki Masuda <masuda@moonmile.net>
2014-04-15 17:29:43 +0900

    Universalを追加

---
+++ XamarinXmlSerialize.Uni/XamarinXmlSerialize.Uni.Shared/BasicPage.xml.Action.cs
@@ -0,0 +1,55 @@
+﻿using XamarinXmlSerialize.Uni.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Xml.Serialization;
+using XamarinXmlSerialize.Core;
+using Windows.Storage;
+
+namespace XamarinXmlSerialize.Uni
+{
+    public sealed partial class BasicPage : Page
+    {
+        private async void ClickLoad(object sender, RoutedEventArgs e)
+        {
+            var se = new XmlSerializer(typeof(MyData));
+            using (var stream = await ApplicationData.Current.LocalFolder.OpenStreamForReadAsync(
+                "mydata.xml"))
+            {
+                var m = se.Deserialize(stream) as MyData;
+                m.CopyTo(_model);
+            }
+        }
+
+        private async void ClickSave(object sender, RoutedEventArgs e)
+        {
+            var se = new XmlSerializer(typeof(MyData));
+            using (var stream = await ApplicationData.Current.LocalFolder.OpenStreamForWriteAsync(
+                "mydata.xml", CreationCollisionOption.OpenIfExists))
+            {
+                stream.SetLength(0);
+                stream.Position = 0;
+                se.Serialize(stream, _model);
+            }
+        }
+
+        MyData _model;
+        private void pageRoot_Loaded(object sender, RoutedEventArgs e)
+        {
+            _model = new MyData();
+            this.DataContext = _model;
+
+        }
+    }
+}
---
+++ XamarinXmlSerialize.WP8/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=391641 を参照してください
+
+namespace XamarinXmlSerialize.WP8
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// このページがフレームに表示されるときに呼び出されます。
+        /// </summary>
+        /// <param name="e">このページにどのように到達したかを説明するイベント データ。
+        /// このプロパティは、通常、ページを構成するために使用します。</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: ここに表示するページを準備します。
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+    }
+}
